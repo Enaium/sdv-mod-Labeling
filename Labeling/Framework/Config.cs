4066e0a2bc28080d9cf1dfe9d2e2de1bde877abe
Enaium <wyy-666@hotmail.com>
2024-03-27 22:40:38 +0800

    update to 1.6

---
+++ Labeling/Framework/Config.cs
@@ -0,0 +1,9 @@
+﻿using StardewModdingAPI;
+
+namespace Labeling.Framework;
+
+public class Config
+{
+    public SButton OpenLabelScreen { get; set; } = SButton.J;
+    public List<Labeling> Labelings { get; set; } = new();
+}
---
+++ Labeling/Framework/Labeling.cs
@@ -0,0 +1,26 @@
+﻿using EnaiumToolKit.Framework.Utils;
+using Microsoft.Xna.Framework;
+using StardewValley;
+
+namespace Labeling.Framework;
+
+public class Labeling
+{
+    public string Name { get; set; }
+    public Vector2 FirstObjectTile { get; set; }
+    public Vector2 SecondObjectTile { get; set; }
+
+    public string CurrentGameLocation { get; set; }
+    public bool Display { get; set; }
+    public ColorUtils.NameType Color { get; set; }
+
+    public Labeling(string name, Vector2 firstObjectTile, Vector2 secondObjectTile, string currentGameLocation, bool display, ColorUtils.NameType color)
+    {
+        Name = name;
+        FirstObjectTile = firstObjectTile;
+        SecondObjectTile = secondObjectTile;
+        CurrentGameLocation = currentGameLocation;
+        Display = display;
+        Color = color;
+    }
+}
